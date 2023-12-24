d4f5248e04142440fa0b632412647c3b3d766e40
Vernicovskiy <victor02vernicovskiy@gmail.com>
2023-12-24 13:44:37 +0300

    Changing brightness by at+led=<Led_Brightness><Enter> or AT+LED=<Led_Brightness><Enter> complete

--- Inc/CMD_Parse_Brightness.h
+++ Inc/CMD_Parse_Brightness.h
@@ -8,7 +8,8 @@
 #ifndef CMD_PARSE_BRIGHTNESS_H_
 #define CMD_PARSE_BRIGHTNESS_H_
 
-#define CMD_PREFIX "AT+LED=" // Префикс для команды
+#define CMD_PREFIX_HIGH_REG "AT+LED=" // Префикс для команды
+#define CMD_PREFIX_LOW_REG "at+led=" // Префикс для команды
 
 int CMD_Parse_Brightness (char *cmd);
 
--- Src/CMD_Parse_Brightness.c
+++ Src/CMD_Parse_Brightness.c
@@ -4,8 +4,8 @@
 
 
 int CMD_Parse_Brightness (char *cmd) {
-  if (strncmp (cmd, CMD_PREFIX, strlen (CMD_PREFIX)) == 0) { // Если команда начинается с префикса
-    return (atoi (cmd + strlen (CMD_PREFIX))); // Возвращаем числовое значение после префикса
+  if ((strncmp (cmd, CMD_PREFIX_HIGH_REG, strlen (CMD_PREFIX_HIGH_REG)) && strncmp (cmd, CMD_PREFIX_LOW_REG, strlen (CMD_PREFIX_LOW_REG))) == 0) { // Если команда начинается с префикса
+    return (atoi (cmd + strlen (CMD_PREFIX_HIGH_REG))); // Возвращаем числовое значение после префикса
   } else { // Если команда не начинается с префикса
     return -1; // Возвращаем -1, как признак ошибки
   }
